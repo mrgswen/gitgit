1275662ec35702a77fa9937a5ae338ba3f3d4618
mrgswen <32538946+mrgswen@users.noreply.github.com>
2017-10-18 19:09:43 +0800

    2

--- src/main/java/eleven/gitgit/App.java
+++ src/main/java/eleven/gitgit/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Heldddlo Worldd!" );
+        System.out.println( "Heldddlo Worldd2!" );
     }
 }
